38e74ebee1d16faec7a17b185d8ae14a9d8181dd
nurwid28 <nur_astutik_24rpl@student.smktelkom-mlg.sch.id>
2017-05-13 22:31:43 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl325/pilmindo/model/Keluarga_model.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl325.pilmindo.model;
+
+/**
+ * Created by Widya on 5/13/2017.
+ */
+
+public class Keluarga_model {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl325/pilmindo/service/GsonGetRequest.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl325.pilmindo.service;
+
+/**
+ * Created by Widya on 5/13/2017.
+ */
+
+public class GsonGetRequest {
+}
